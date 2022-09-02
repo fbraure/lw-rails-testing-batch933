d0f256c5f136c8b7d8d5441176159cdd823b4990
Florent BRAURE <florent.braure@gmail.com>
2022-09-02 10:00:26 +0200

    salut

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'products/index'
   devise_for :users
   root to: "products#index" # And remove the generated route `products/index`
+  resources :products, only: [:new, :create]
 end
